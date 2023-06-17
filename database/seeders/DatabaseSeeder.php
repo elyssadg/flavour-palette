9522d8456325a8528475c69ae3034a0a1e7c90b6
guidowilliam11 <guido.william@binus.ac.id>
2023-06-17 23:24:15 +0700

    feats : comment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,45 +7,47 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User
-        $userSeeder = new UserSeeder();
-        $userId = $userSeeder->run();
+        // // User
+        // $userSeeder = new UserSeeder();
+        // $userId = $userSeeder->run();
 
-        // Seller
-        $cateringSeeder = new SellerSeeder();
-        $cateringId = $cateringSeeder->run($userId);
+        // // Seller
+        // $cateringSeeder = new SellerSeeder();
+        // $cateringId = $cateringSeeder->run($userId);
 
-        // Customer
-        $customerSeeder = new CustomerSeeder();
-        $customerId = $customerSeeder->run($userId);
+        // // Customer
+        // $customerSeeder = new CustomerSeeder();
+        // $customerId = $customerSeeder->run($userId);
 
-        // Menu
-        $menuSeeder = new MenuSeeder();
-        $menuId = $menuSeeder->run($cateringId);
+        // // Menu
+        // $menuSeeder = new MenuSeeder();
+        // $menuId = $menuSeeder->run($cateringId);
 
-        // Review
-        $reviewSeeder = new MenuReviewSeeder();
-        $reviewSeeder->run($menuId, $customerId);
+        // // Review
+        // $reviewSeeder = new MenuReviewSeeder();
+        // $reviewSeeder->run($menuId, $customerId);
 
-        // Category
-        $categorySeeder = new CategorySeeder();
-        $categoryId = $categorySeeder->run();
+        // // Category
+        // $categorySeeder = new CategorySeeder();
+        // $categoryId = $categorySeeder->run();
 
-        // MenuCategory
-        $menuCategorySeeder = new MenuCategorySeeder();
-        $menuCategorySeeder->run($menuId, $categoryId);
+        // // MenuCategory
+        // $menuCategorySeeder = new MenuCategorySeeder();
+        // $menuCategorySeeder->run($menuId, $categoryId);
 
-        // OrderHeader
-        $orderHeaderSeeder = new OrderHeaderSeeder();
-        $orderHeaderSeeder->run($customerId, $cateringId);
-        $menuWeekHeaderSeeder = new MenuWeekHeaderSeeder();
-        $menuWeekHeaderId = $menuWeekHeaderSeeder->run();
-        $menuWeekDetailSeeder = new MenuWeekDetailSeeder();
-        $menuWeekDetailSeeder->run($menuWeekHeaderId, $menuId);
+        // // OrderHeader
+        // $orderHeaderSeeder = new OrderHeaderSeeder();
+        // $orderHeaderSeeder->run($customerId, $cateringId);
+
+        // $menuWeekHeaderSeeder = new MenuWeekHeaderSeeder();
+        // $menuWeekHeaderId = $menuWeekHeaderSeeder->run();
+        // $menuWeekDetailSeeder = new MenuWeekDetailSeeder();
+        // $menuWeekDetailSeeder->run($menuWeekHeaderId, $menuId);
     }
 }
--- database/seeders/MenuWeekDetailSeeder.php
+++ database/seeders/MenuWeekDetailSeeder.php
@@ -9,43 +9,45 @@ use Faker\Factory as Faker;
 
 class MenuWeekDetailSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      */
     public function run($menuWeekHeaderId, $menuId)
     {
-        $faker = Faker::create();
+        // $faker = Faker::create();
 
-        $startDate = strtotime('2023-06-19');
-        $dateStarts = [];
+        // $startDate = strtotime('2023-06-19');
+        // $dateStarts = [];
 
-        for ($i = 0; $i < 3600; $i++) {
-            $date = date('Y-m-d', strtotime('+' . $i . ' days', $startDate));
-            $dateStarts[] = $date;
-        }
+        // for ($i = 0; $i < 3600; $i++) {
+        //     $date = date('Y-m-d', strtotime('+' . $i . ' days', $startDate));
+        //     $dateStarts[] = $date;
+        // }
 
-        $j=0;
-        $counter=0;
-        for ($i = 0; $i < 1; $i++) {
-            for ($j = 0; $j < 7; $j++){
-                $randomNumbers = [];
-                for ($k = 0; $k < 20; $k++) {
-                    $randomNumber = rand(0, 50);
-                    while (in_array($randomNumber, $randomNumbers)) {
-                        $randomNumber = rand(1, 50);
-                    }
+        // $j=0;
+        // $counter=0;
+        // for ($i = 0; $i < 1; $i++) {
+        //     for ($j = 0; $j < 7; $j++){
+        //         $randomNumbers = [];
+        //         for ($k = 0; $k < 20; $k++) {
+        //             $randomNumber = rand(0, 50);
+        //             while (in_array($randomNumber, $randomNumbers)) {
+        //                 $randomNumber = rand(1, 50);
+        //             }
 
-                    $randomNumbers[$k] = $randomNumber;
-                }
-                for($l = 0; $l < $faker->numberBetween(3, 15); $l++){
-                    MenuWeekDetail::create([
-                        'week_id' => $menuWeekHeaderId[$i],
-                        'menu_id' => $menuId[$randomNumbers[$l]],
-                        'available_date' => $dateStarts[$counter]
-                    ]);
-                }
-                $counter++;
-            }
-        }
+        //             $randomNumbers[$k] = $randomNumber;
+        //         }
+        //         for($l = 0; $l < $faker->numberBetween(3, 15); $l++){
+        //             MenuWeekDetail::create([
+        //                 'week_id' => $menuWeekHeaderId[$i],
+        //                 'menu_id' => $menuId[$randomNumbers[$l]],
+        //                 'available_date' => $dateStarts[$counter]
+        //             ]);
+        //         }
+        //         $counter++;
+        //     }
+        // }
+
     }
 }
