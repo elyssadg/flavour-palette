f35736a308a8ed8314e7003d0a7ded0f4b285ce3
guidowilliam11 <guido.william@binus.ac.id>
2023-06-20 00:16:33 +0700

    feats : fix image ratio

--- resources/views/pages/order_detail.blade.php
+++ resources/views/pages/order_detail.blade.php
@@ -37,43 +37,43 @@
             @foreach ($orderHeader->order_detail as $od)
                 <div class="flex justify-between gap-5 items-start w-full">
                     <div class="w-[30%] rounded mb-auto">
-                        <img class="w-full h-80 object-cover rounded shadow" src="{{ Storage::url("profile/menu/".$od->menu->profile_menu) }}"/>
+                        <img class="w-full aspect-square object-cover rounded shadow" src="{{ Storage::url("profile/menu/".$od->menu->profile_menu) }}"/>
                     </div>
                     <div class="flex flex-col gap-3 w-[65%]">
                         <div class="flex flex-col gap-2">
                             <div class="text-primary text-name font-normal">Menu Name</div>
                             <div class="text-secondary text-subheading font-medium">{{ $od->menu->name }}</div>
                         </div>
-    
+
                         @if (Auth::user()->role == 'customer')
                             <div class="flex flex-col gap-2">
                                 <div class="text-primary text-name font-normal">Seller</div>
                                 <div class="text-secondary text-subheading font-medium">{{ $od->menu->seller->name }}</div>
                             </div>
                         @endif
-    
+
                         <div class="flex flex-col gap-2">
                             <div class="text-primary text-name font-normal">Quantity</div>
                             <div class="text-secondary text-subheading font-medium">{{ $od->quantity }}</div>
                         </div>
-    
+
                         <div class="flex flex-col gap-2">
                             <div class="text-primary text-name font-normal">Subtotal</div>
                             <div class="text-secondary text-subheading font-medium">Rp{{ number_format(($od->quantity * $od->menu->price)/1000, 3, '.', ',') }},00</div>
                         </div>
-    
+
                         <div class="flex flex-col gap-2">
                             <div class="text-primary text-name font-normal">Delivery Date</div>
                             <div class="text-secondary text-subheading font-medium">{{ date('d/m/Y', strtotime($od->arrival_date)) }}</div>
                         </div>
-    
+
                         <div class="flex flex-col gap-2">
                             <div class="text-primary text-name font-normal">Status</div>
                             <div class="w-fit py-1 px-4 border border-secondary text-secondary font-medium rounded flex items-center justify-center">
                                 {{ $od->status }}
                             </div>
                         </div>
-                        
+
                         <div class="mt-5 ml-auto flex justify-center">
                             <form action="/order/status" method="POST">
                                 {{ @csrf_field() }}
